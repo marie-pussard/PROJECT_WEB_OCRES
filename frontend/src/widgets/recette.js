6b5cb25868b95ef7cf76ade71ed8854cb7cf678a
Jakob Zettel <jakobzettel@Jakobs-MacBook-Pro.local>
2020-11-29 15:21:10 +0100

    RecipeWidget started, API included

--- frontend/src/widgets/recette.js
+++ frontend/src/widgets/recette.js
@@ -2,7 +2,10 @@ import './recette.css';
 import React from 'react';
 import axios from "axios";
 
-const API_URL = "https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2";
+const API_KEY = "516eb264698844f582cdb9bc1277dd93";
+const API_URL = "https://api.spoonacular.com/recipes/complexSearch";
+//const API_URL = "https://api.spoonacular.com/recipes/716429/information";
+const API_IMAGE_URL = "https://spoonacular.com/recipeImages/";
 
 
 export class Recipe extends React.Component {
@@ -20,60 +23,94 @@ export class Recipe extends React.Component {
     .then((response) => {
       // Récupère la donnée d'une API
       const data = response.data;
+      console.log(data);
+      
       // On récupère l'information principal
-      /*
-      const main = data.weather[0].main;
-      const temperature = data.main.temp;
+      const titleOne = data.results[0].title;
+      const titleTwo = data.results[1].title;
+      const titleThree = data.results[2].title;
+      //const recipeIdOne = data.results[0].id;
+      //const recipeIdTwo = data.results[1].id;
+      //const recipeIdThree = data.results[2].id;
+      const imageURLone = data.results[0].image;
+      const imageURLtwo = data.results[1].image;
+      const imageURLthree = data.results[2].image;
+
+      const descriptionOne = data.results[0]
+      //const nutOne = data.results[0].nutrition.nutirents[0].title;
+      const CaloriesOne = data.results[0].nutrition.nutrients[0].amount;
+      const CaloriesOneUnit = data.results[0].nutrition.nutrients[0].unit;
+      const CaloriesTwo = data.results[1].nutrition.nutrients[0].amount;
+      const CaloriesTwoUnit = data.results[1].nutrition.nutrients[0].unit;
+      const CaloriesThree = data.results[2].nutrition.nutrients[0].amount;
+      const CaloriesThreeUnit = data.results[2].nutrition.nutrients[0].unit;
+      /*const temperature = data.main.temp;
       const humidity = data.main.humidity;
       const icon = data.weather[0].icon;
       const windspeed = data.wind.speed;
       /*write information in state*/
       /*this.setState({today_forecast_main:main}); /* ? */
-      /*this.setState({temperature});
-      this.setState({icon});
-      this.setState({windspeed});
-      this.setState({humidity});*/
-      console.log(main);
+      this.setState({titleOne});
+      this.setState({titleTwo});
+      this.setState({titleThree});
+      this.setState({CaloriesOne});
+      this.setState({CaloriesOneUnit});
+      this.setState({CaloriesTwo});
+      this.setState({CaloriesTwoUnit});
+      this.setState({CaloriesThree});
+      this.setState({CaloriesThreeUnit});
+
+      this.setState({imageURLone});
+      this.setState({imageURLtwo});
+      this.setState({imageURLthree});
+      //console.log(title);
   })
 }
   fetchRecipe(){
     return axios
-    .get(`${API_URL}`, {
-    /*.get(`${API_URL}?lat=${this.state.lat}&lon=${this.state.lon}&units=metric&appid=${API_KEY}`, {*/
+    //assemble API with Key and set nutrition parameters
+   // .get(`${API_URL}?apiKey=${API_KEY}&includeNutrition=true`, {
+      .get(`${API_URL}?apiKey=${API_KEY}&query=pasta&maxFat=25&maxSugar=30&minProtein=25&maxCalories=600&number=3`, {
       crossdomain: true
     })
   }
 
-  getHTMLElementFromIcon(icon){
-    //return `<img src=${API_URL}${icon}@2x.png class="weather-icon"/>`
+  getHTMLElementFromImage(image){
+    //return `<img src=${API_IMAGE_URL}${recipeID}-312x231@2x.jpg class="weather-icon"/>`
   }
 
   render(){
     return(
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="..." alt="First slide"></img>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <span className="titel">{this.state.titleOne}</span>
+            <span className="conditions"><img src={`${this.state.imageURLone}`}></img></span>
+            <img className="d-block w-100" src="..." alt="First slide"></img>
           </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Second slide"></img>
+          <div className="carousel-item">
+          <span className="titel">{this.state.titleTwo}</span>
+          <span className="conditions"><img src={`${this.state.imageURLone}`}></img></span>
+            <img className="d-block w-100" src="..." alt="Second slide"></img>
           </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Third slide"></img>
+          <div className="carousel-item">
+          <span className="titel">{this.state.titleThree}</span>
+          <span className="conditions"><img src={`${this.state.imageURLone}`}></img></span>
+            <img className="d-block w-100" src="..." alt="Third slide"></img>
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
     )
