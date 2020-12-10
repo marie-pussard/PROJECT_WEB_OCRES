a90d8b7f88117edf464cd4420aa7dffb49925165
Jakob Zettel <jakobzettel@Jakobs-MacBook-Pro.local>
2020-12-10 19:48:31 +0100

    InputPage started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     <div className="App">
       <main className="container">  
         {/* <Meteo/>*/}
-        <Recipe/>
+        {/*<Recipe/>*/}
       </main>
     </div>
   );
