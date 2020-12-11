18e547789b21ce366eb111fad3caf3a2f299ef13
Jakob Zettel <jakobzettel@Jakobs-MacBook-Pro.local>
2020-12-11 23:26:34 +0100

    form react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 // import {Meteo} from './widgets/meteo';
-import {Recipe} from './widgets/recette';
+// import {Recipe} from './widgets/recette';
 
-class App extends React.Component {
-  constructor(props){
-    super(props)
+const axios = require('axios');
+
+function App() {
+  const [showAdmin, setAdmin] = useState(false);
+  const [sexe, setSexe] = useState();
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    event.stopPropagation();
+    axios.post('/form-submit', { sexe}) //weight, sporttype, hoursSporttype, daysPerWeek 
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+  function handleSexeChange(e){
+    setSexe(e.target.value);
   }
 
-  render(){
   return (
-    <div className="App">
-      <main className="container">  
-        {/* <Meteo/>*/}
-        {/*<Recipe/>*/}
-      </main>
+    <div>
+      <Navbar bg="dark" expand="lg">
+        <Button className="m-2" onClick={() => setAdmin(!showAdmin)}>
+          {showAdmin ? 'Back' : 'Login'}
+        </Button>
+        {//{showAdmin ?  <h3>    Please fill in the following form !</h3> : <p1></p1>}
+        }   
+      </Navbar>
+      <div className="container mt-5">
+      {showAdmin ?
+      <div>
+        <h3>Please fill in the following form.</h3>
+        <Form.Group controlId="formBasicSexe">
+            <Form.Label>Gender</Form.Label>
+            {['Male', 'Female', 'Divers'].map(label => (
+              <Form.Check
+                key={label}
+                id={label}
+                type="radio"
+                name="sexe"
+                value={label}
+                label={label}
+                checked={sexe === label}
+                onChange={handleSexeChange}
+              />
+            ))}
+          </Form.Group>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicWeight">
+              <Form.Label>Your weight</Form.Label>
+              <Form.Control type="number" placeholder="Your weight" />
+              <Form.Text className="text-muted">
+              </Form.Text>
+              <Form.Check label="Share this data with third parties."/>
+            </Form.Group>
+            <Form.Group controlId="formBasicSporttype">
+              <Form.Label>Sporttype</Form.Label>
+              <Form.Control type="text" placeholder="Sporttype" />
+            </Form.Group>
+            <Form.Group controlId="formBasicSporttype">
+              <Form.Label>How many hours you practiced this sporttype?</Form.Label>
+              <Form.Control type="number" placeholder="Hours per Sporttype" />
+            </Form.Group>
+            <Form.Group controlId="formBasicSporttype">
+              <Form.Label>How many days you practiced per week?</Form.Label>
+              <Form.Control type="number" placeholder="Days per week" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+          </div>
+          :
+          <Jumbotron>
+            <h1>Welcome</h1>
+            <p>
+              To use this site as an Admin, please log in by using the button in the top left corner.
+            </p>
+          </Jumbotron>
+      }
+      </div>
     </div>
   );
-}
 }
 
 export default App;
